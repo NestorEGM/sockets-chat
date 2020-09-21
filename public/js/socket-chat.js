bfe8bbc9a43660d3ed4dc211bf6dc4f5b763edef
Nestor <nestor_gm02@hotmail.com>
2020-09-21 15:51:46 -0500

    chat finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,48 +1,60 @@
 var socket = io();
 
-const params = new URLSearchParams(window.location.search);
+var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y sala son necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
 
-const usuario = {
+var usuario = {
     nombre: params.get('nombre'),
-    sala: params.get('sala'),
+    sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function () {
-    socket.emit('entrarChat', usuario, resp => {
-        console.log('Usuarios conectados', resp);
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function (resp) {
+        // console.log('Usuarios conectados', resp);
+        renderizatUsuarios(resp);
     });
+
 });
 
 // escuchar
 socket.on('disconnect', function () {
+
     console.log('Perdimos conexión con el servidor');
+
 });
 
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
-// }, function (resp) {
+// }, function(resp) {
 //     console.log('respuesta server: ', resp);
 // });
 
 // Escuchar información
 socket.on('crearMensaje', function (mensaje) {
-    console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-// Escuchar cuando un usuario entra o sale del chat
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function (personas) {
-    console.log(personas);
+    renderizatUsuarios(personas);
 });
 
 // Mensajes privados
-socket.on('mensajePrivado', (mensaje) => {
-    console.log('Mensaje privado:', mensaje);
+socket.on('mensajePrivado', function (mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
